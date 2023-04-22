1f14e68cbd11253f9dbf7ef1b3fc36dd6b2e8e51
Fabian Torres <programacioncursos2022@gmail.com>
2023-04-22 12:35:48 -0500

    Add new functions

--- funciones.py
+++ funciones.py
@@ -1,22 +1,44 @@
-import random
-import string
-import bcrypt
+import random, string, bcrypt, sys
+from time import sleep
+
+def output(string):
+    for i in string:
+        print(i, end="")
+        sys.stdout.flush()
+        sleep(0.04)
+    print("\n")
+def footer():
+    output("---------------------------Hecho con Amor por Fabián---------------------------")
+def config_contraseña(longitud_contraseña):
+    while True:
+        config = input("¿Cómo quieres que sea tu contraseña (Sólo digitos/Sólo letras/Alfanumérica)?: ")
+        if  config.lower() == 'sólo dígitos' or config.lower() == 'solo digitos' or config.lower() == 'sólo digitos' or config.lower() == 'solo dígitos':
+            output(generar_contraseña(longitud=longitud_contraseña, letras=False))
+            break
+        elif config.lower() == 'sólo letras' or config.lower() == 'solo letras':
+            output(generar_contraseña(longitud=longitud_contraseña, digitos=False))
+            break
+        elif config.lower() == 'alfanumerica' or config.lower() == 'alfanumérica':
+            output(generar_contraseña(longitud=longitud_contraseña))
+            break
+        else:
+            output("Lo siento, no entendí lo que quisiste decir.")
+            continue
+
 def generar_contraseña(longitud=12, digitos=True, letras=True):
     password = ""
     posiciones = [1,2]
-    while longitud >= 4 and longitud <= 40:
-        while len(password) < longitud:
-            tipo_caracter = random.choice(posiciones)
-            if digitos and letras:
-                if tipo_caracter == 1:
-                    password += random.choice(string.digits)
-                else:
-                    password += random.choice(string.ascii_letters)
-            elif digitos:
+    while len(password) < longitud:
+        tipo_caracter = random.choice(posiciones)
+        if digitos and letras:
+            if tipo_caracter == 1:
                 password += random.choice(string.digits)
             else:
                 password += random.choice(string.ascii_letters)
-        break
+        elif digitos:
+            password += random.choice(string.digits)
+        else:
+            password += random.choice(string.ascii_letters)
     return password
 
 def hashear(password):
@@ -30,6 +52,7 @@ def guardar_hash(password_hash):
     f = open('Credenciales.txt','a')
     f.writelines(f'Hash: {password_hash}\n')
     f.close()
+
 
 
 
